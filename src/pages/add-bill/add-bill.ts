68324df3ddea76ad14a3afd75ca5004e2133c88a
albert <790321193@qq.com>
2017-04-12 12:46:41 +0800

    index页面

--- src/pages/add-bill/add-bill.ts
+++ src/pages/add-bill/add-bill.ts
@@ -5,7 +5,21 @@ import { Component } from '@angular/core';
   templateUrl: 'add-bill.html'
 })
 export class AddBillPage {
-  constructor() {
+	icons: string[];
+  	items: Array<{title: string, amount: string,time:string, icon: string}>;
 
-  }
+  	constructor() {
+	    this.icons = ['flask', 'wifi', 'beer', 'football', 'basketball', 'paper-plane',
+	    'american-football', 'boat', 'bluetooth', 'build'];
+
+	    this.items = [];
+	    for(let i = 1; i < 4; i++) {
+	      this.items.push({
+	        title: '零食小吃 ' + i,
+	        amount:  "￥"+i+".00元",
+	        time:'09:15',
+	        icon: this.icons[Math.floor(Math.random() * this.icons.length)]
+	      });
+	     }
+ 	}
 }
