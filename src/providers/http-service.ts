e4d872030eaf8c6b5ea623e158e517fc2d41ec88
albert <790321193@qq.com>
2017-05-03 16:58:46 +0800

    ---

--- src/providers/http-service.ts
+++ src/providers/http-service.ts
@@ -9,7 +9,7 @@ import { RestEntity } from '../domain/RestEntity';
 
 @Injectable()
 export class HttpService {
-    hostUrl:string = "http://122.225.97.133:9971";
+    hostUrl:string = "http://61.174.50.10:9971";
     constructor(
         private http: Http,
         public loadingCtrl: LoadingController 
